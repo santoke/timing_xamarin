732e565fafc340c4ece06c2b7791353f07eb4742
good_cho <cho.cho@goodoc.co.kr>
2019-02-13 17:47:47 +0900

    UI Test 코드

---
+++ UITest1/AppInitializer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace UITest1
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp.Android.ApkFile(@"C:\Users\cho\source\repos\timing_xamarin\timing\timing.Android\bin\Release\com.companyname.timing-Signed.apk")
+                    .PreferIdeSettings()
+                    .EnableLocalScreenshots()
+                    .StartApp();
+            }
+
+            return ConfigureApp.iOS.StartApp();
+        }
+    }
+}
---
+++ UITest1/view/MainPage.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace UITest1.view
+{
+    [TestFixture(Platform.Android)]
+    class MainPage
+    {
+        IApp app;
+        Platform platform;
+
+        public MainPage(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void IsThereLoginButtons()
+        {
+            //AppResult[] results = app.WaitForElement(c => c.Marked("Welcome to Xamarin.Forms!"));
+            var results = app.Query(c => c.Button());
+            Assert.IsTrue(results.Length == 3);
+        }
+
+        [Test]
+        public void ClickFacebook()
+        {
+
+        }
+    }
+}
